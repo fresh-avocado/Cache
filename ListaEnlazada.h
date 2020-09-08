724e4bed3f49907516e8f1d5d3a36ae47734d238
Gabriel <gabriel.spranger@utec.edu.pe>
2020-09-08 11:24:43 -0500

    initial commit

---
+++ ListaEnlazada.h
@@ -0,0 +1,129 @@
+//
+// Created by Gabriel Spranger Rojas on 9/6/20.
+//
+
+#ifndef TESTENTRADA_LISTAENLAZADA_H
+#define TESTENTRADA_LISTAENLAZADA_H
+
+#include <iostream>
+
+// TODO: namespace this shit or declare Node inside ListaEnlazada
+
+using namespace std;
+
+#define node Node<U, T>
+
+template <typename U, typename T>
+class Node;
+
+template <typename U, typename T>
+class ListaEnlazada {
+    node* head;
+    node* tail;
+public:
+    ListaEnlazada(): head{nullptr}, tail{nullptr} {}
+
+    node* getHead() { // O(1)
+        return head;
+    }
+
+    void pushFront(node* newNode) { // O(1)
+        if (!newNode) return;
+        node* aux = head;
+        newNode->next = head;
+        if (head) {
+            head->prev = newNode;
+        }
+        head = newNode;
+        if (!aux) { // first element was inserted
+            tail = newNode;
+        }
+    }
+
+    void pushBack(node* newNode) { // O(1)
+        if (tail) {
+            tail->next = newNode;
+            newNode->prev = tail;
+            tail = newNode;
+        } else {
+            head = newNode;
+            tail = newNode;
+        }
+    }
+
+    U getTailKey() {
+        return tail->key.first;
+    }
+
+    U getHeadKey() {
+        return head->key.second;
+    }
+
+    void deleteByAddress(node* nodeToDelete) { // O(1)
+        // ojo que este metodo no llama a delete
+        // por lo q si se quiere borrar el nodo definitivamente
+        // el usuario del API debe hacerlo manualmente
+        if (!nodeToDelete) return;
+        if (nodeToDelete->prev) {
+            nodeToDelete->prev->next = nodeToDelete->next;
+        } else {
+            head = nodeToDelete->next;
+        }
+        if (nodeToDelete->next) {
+            nodeToDelete->next->prev = nodeToDelete->prev;
+        }
+    }
+
+    void deleteTail() {
+        deleteByAddress(tail);
+    }
+
+    void deleteHead() {
+        deleteByAddress(head);
+    }
+
+    // used for debug purposes
+    void print() {
+        node* it = head;
+        while (it) {
+            cout << it->key.first << " ";
+            it = it->next;
+        }
+        cout << "\n";
+    }
+
+    ~ListaEnlazada() { // O(CacheMejorado.maxSize)
+        node* aux1 = head;
+        node* aux2 = head;
+        while (aux2) {
+            aux2 = aux1->next;
+            delete aux1;
+            aux1 = aux2;
+        }
+    }
+
+};
+
+template <typename U, typename T>
+class Node {
+    pair<U, T> key;
+    node* prev;
+    node* next;
+public:
+    explicit Node(pair<U, T> v): key{v}, prev{nullptr}, next{nullptr} {}
+
+    void updateKey(pair<U, T> newKey) {
+        key = newKey;
+    }
+
+    pair<U, T> getKey() {
+        return key;
+    }
+
+    template <typename V, typename W>
+    friend class ListaEnlazada;
+};
+
+
+
+#endif //TESTENTRADA_LISTAENLAZADA_H
---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include "CacheMejorado.h"
+
+using namespace std;
+
+int main() {
+
+//  CacheMejorado<FIFO, string, int> lruCache(3);
+    CacheMejorado<LRU, string, int> lruCache(3); // int porque nuestro cache tendra keys de tipo string, guardara int's como values y usara el policy de LRU
+
+    cout << lruCache.getMostRecentKey().value_or("No recent key") << endl; //
+
+    // testear operaciones
+    lruCache.insertKeyValuePair("b", 2);
+    lruCache.insertKeyValuePair("a", 1);
+    lruCache.insertKeyValuePair("c", 3);
+
+    cout << lruCache.getMostRecentKey().value_or("No recent key") << endl; // c
+
+    cout << lruCache.getValueFromKey("a").value_or(INT_MIN) << endl; // 1
+
+    cout << lruCache.getMostRecentKey().value_or("No recent key") << endl; // a
+
+    lruCache.insertKeyValuePair("d", 4);
+
+    cout << lruCache.getMostRecentKey().value_or("No recent key") << endl; // d
+
+    cout << lruCache.getValueFromKey("b").value_or(INT_MIN) << endl;
+
+    lruCache.insertKeyValuePair("a", 5);
+
+    cout << lruCache.getValueFromKey("a").value_or(INT_MIN) << endl; // 5
+
+
+////    Probando la implementacion de la lista enlazada
+//    ListaEnlazada<string, int> listaEnlazada;
+//
+//    auto* node1 = new Node<string, int>(make_pair("a", 1));
+//    listaEnlazada.pushBack(node1);
+//    auto* node2 = new Node<string, int>(make_pair("b", 2));
+//    listaEnlazada.pushBack(node2);
+//    auto* node3 = new Node<string, int>(make_pair("c", 3));
+//    listaEnlazada.pushFront(node3);
+//    auto* node4 = new Node<string, int>(make_pair("d", 4));
+//    listaEnlazada.pushBack(node4);
+//    auto* node5 = new Node<string, int>(make_pair("e", 5));
+//    listaEnlazada.pushBack(node5);
+//
+//    listaEnlazada.print();
+//    listaEnlazada.deleteByAddress(node3);
+//    listaEnlazada.print();
+//    listaEnlazada.pushFront(node3);
+//    listaEnlazada.print();
+
+    return 0;
+}
